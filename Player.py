3f7fa3ab9908c5f349872009cf84087c3c835822
Andreas Lüthi <andreas.luethi@gmx.net>
2020-04-25 15:48:53 +0200

    Add progress bar.

--- Player.py
+++ Player.py
@@ -5,6 +5,8 @@ import sched, time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ('localhost', 10110)
 
+log_file = "Vega-07.log"
+
 
 ##  17939076 <$GPRMC,135218.00,A,4703.84791,N,00654.17033,E,82.429,238.68,300706,,,A*50>
 def toNMEA(line):
@@ -17,12 +19,23 @@ def toNMEA(line):
 def send(nmea):
     sock.sendto(f'{nmea}\n'.encode(), addr)
 
+## stolen from: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+def progress(count, total, suffix=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+    print('\r[%s] %s%s ...%s' % (bar, percents, '%', suffix), end='', flush=True)
+
 
 sched = sched.scheduler(time.time, time.sleep)
 startTime = time.time()
 baseTime = None
-with open("Vega-07.log", "r") as logfile:
+num_lines = sum(1 for line in open(log_file))
+
+with open(log_file, "r") as logfile:
     for cnt, line in enumerate(logfile):
+        progress(cnt, num_lines)
         ts, nmea, = toNMEA(line)
         if baseTime is None:
             baseTime = ts
