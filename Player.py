178f769b128808333aa9a3cbf694c2f667c3ddeb
Andreas Lüthi <andreas.luethi@gmx.net>
2020-04-26 10:49:31 +0200

    Cosmetics.

--- Player.py
+++ Player.py
@@ -7,15 +7,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ('localhost', 10110)
 
 parser = argparse.ArgumentParser(
-    "python NMAE Player",
-    description="Replay a nmae logfile with timestamp to udp (localhost, 10110).")
+    'python NMAE Player',
+    description='Replay a nmae logfile with timestamp to udp (localhost, 10110).')
 parser.add_argument('logfile', metavar='log-file', type=str, )
 results = parser.parse_args()
 
 
 ##  17939076 <$GPRMC,135218.00,A,4703.84791,N,00654.17033,E,82.429,238.68,300706,,,A*50>
 def toNMEA(line):
-    parts = (" ".join(line.split()).split())
+    parts = (' '.join(line.split()).split())
     ts = int(parts[0])
     nmea = parts[1][1:-1]
     return ts, nmea
@@ -43,9 +43,9 @@ num_lines = sum(1 for line in open(log_file))
 with open(log_file, "r") as logfile:
     for cnt, line in enumerate(logfile):
         ts, nmea, = toNMEA(line)
+        assert ts > 0, 'Wrong time increment'
         progress(cnt, num_lines, ts)
         if baseTime is None:
             baseTime = ts
-        assert ts > 0, "Wrong time increment"
         sched.enterabs(startTime + (float(ts - baseTime)) / 1000.0, 1, lambda x: send(x), (nmea,))
         sched.run()
