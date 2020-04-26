04609f122e7182598a3c863e8b01179af44489c1
Andreas Lüthi <andreas.luethi@gmx.net>
2020-04-26 10:46:04 +0200

    Add argument parser.

--- Player.py
+++ Player.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 import socket
 import sched, time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 addr = ('localhost', 10110)
 
-log_file = "Vega-07.log"
+parser = argparse.ArgumentParser(
+    "python NMAE Player",
+    description="Replay a nmae logfile with timestamp to udp (localhost, 10110).")
+parser.add_argument('logfile', metavar='log-file', type=str, )
+results = parser.parse_args()
 
 
 ##  17939076 <$GPRMC,135218.00,A,4703.84791,N,00654.17033,E,82.429,238.68,300706,,,A*50>
@@ -19,6 +24,7 @@ def toNMEA(line):
 def send(nmea):
     sock.sendto(f'{nmea}\n'.encode(), addr)
 
+
 ## stolen from: https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
 def progress(count, total, suffix=''):
     bar_len = 60
@@ -31,12 +37,13 @@ def progress(count, total, suffix=''):
 sched = sched.scheduler(time.time, time.sleep)
 startTime = time.time()
 baseTime = None
+log_file = results.logfile
 num_lines = sum(1 for line in open(log_file))
 
 with open(log_file, "r") as logfile:
     for cnt, line in enumerate(logfile):
-        progress(cnt, num_lines)
         ts, nmea, = toNMEA(line)
+        progress(cnt, num_lines, ts)
         if baseTime is None:
             baseTime = ts
         assert ts > 0, "Wrong time increment"
